6ea193a39d83185d34a04138b5933f76b4a3d83b
unknown <yelemo777@gmail.com>
2022-10-05 20:12:15 -0500

    Creacion de signin y toda su logica

--- django_crud_fazt/urls.py
+++ django_crud_fazt/urls.py
@@ -22,7 +22,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/',views.tasks, name='tasks'),
-    path('logout/',views.singout, name='logout'),
+    path('logout/',views.signout, name='logout'),
+    path('signin/',views.signin, name='signin'),
+
 
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm  # crear formularios
+# crear formularios
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 # Create your views here.
 
@@ -28,15 +29,36 @@ def signup(request):
                     'form': UserCreationForm,
                     'error': "Username already exists"})
 
-                
         return render(request, 'signup.html', {
             'form': UserCreationForm,
             'error': "Password do not match"})
+
 
 def tasks(request):
     return render(request, 'tasks.html')
 
 
-def singout(request):
+def signout(request):
     logout(request)
     return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+            'form': AuthenticationForm,
+            'error' : 'Username or password is incorrect'
+           })
+        else:
+            login(request, user)
+            return redirect('tasks')
+       
+        
+
