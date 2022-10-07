fb0064b7087aee13f749f79431d88a342047f2cd
unknown <yelemo777@gmail.com>
2022-10-07 18:17:17 -0500

    Formulario de tareas

--- django_crud_fazt/urls.py
+++ django_crud_fazt/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('tasks/',views.tasks, name='tasks'),
     path('logout/',views.signout, name='logout'),
     path('signin/',views.signin, name='signin'),
-
-
+    path('tasks/create/',views.create_task, name='create_task')
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
 # Create your views here.
 
 
@@ -37,6 +38,19 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+def create_task(request):
+    if request.method == 'GET':
+         return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+    else:
+        print(request.POST)
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+
+
+   
 
 def signout(request):
     logout(request)
