46eb94baf5ea985d7129837f2b43846181473c52
unknown <yelemo777@gmail.com>
2022-10-11 22:54:55 -0500

    Proteger rutas

--- tasks/views.py
+++ tasks/views.py
@@ -8,6 +8,7 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -37,15 +38,17 @@ def signup(request):
             'form': UserCreationForm,
             'error': "Password do not match"})
 
-
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user = request.user, datecompleted__isnull=True) 
     return render(request, 'tasks.html',{'tasks': tasks})
 
+@login_required
 def tasks_completed(request):
     tasks = Task.objects.filter(user = request.user, datecompleted__isnull=False).order_by('datecompleted')
     return render(request, 'tasks.html',{'tasks': tasks})
 
+@login_required
 def create_task(request):
     if request.method == 'GET':
          return render(request, 'create_task.html', {
@@ -65,7 +68,7 @@ def create_task(request):
                 })
 
    
-
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
@@ -87,7 +90,8 @@ def signin(request):
         else:
             login(request, user)
             return redirect('tasks')
-       
+
+@login_required       
 def task_detail(request, task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user = request.user)
@@ -106,6 +110,7 @@ def task_detail(request, task_id):
             'task' : task, 'form' : form, 'error' : "Error updating task."
             })   
 
+@login_required
 def complete_task(request, task_id):
     task =  get_object_or_404(Task, pk = task_id,user = request.user)
     if request.method == 'POST':
@@ -113,6 +118,7 @@ def complete_task(request, task_id):
         task.save()
         return redirect('tasks')
 
+@login_required
 def delete_task(request, task_id):
     task =  get_object_or_404(Task, pk = task_id,user = request.user)
     if request.method == 'POST':
