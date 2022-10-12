5de7374ba3366b9d37685b0bc81a6fbe0024b816
unknown <yelemo777@gmail.com>
2022-10-11 21:49:00 -0500

    completar y eliminar tarea

--- tasks/views.py
+++ tasks/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 # Create your views here.
 
 
@@ -101,4 +102,15 @@ def task_detail(request, task_id):
             'task' : task, 'form' : form, 'error' : "Error updating task."
             })   
 
+def complete_task(request, task_id):
+    task =  get_object_or_404(Task, pk = task_id,user = request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
 
+def delete_task(request, task_id):
+    task =  get_object_or_404(Task, pk = task_id,user = request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
